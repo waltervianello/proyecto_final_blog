fc1161b66bd397abd59c71840fe2a5241fa1ee0d
Walter Vianello <waltervianello@gmail.com>
2022-10-20 18:18:48 -0300

    el primer commit del proyecto

---
+++ theblog/urls.py
@@ -0,0 +1,10 @@
+from re import A
+from django.urls import path
+#from.import views
+from.views import AddPostView, HomeView,ArticleDetailView
+
+urlpatterns = [
+    path("",HomeView.as_view(),name="home"),
+    path("article/<int:pk>",ArticleDetailView.as_view(),name="article_detail"),
+    path("add_post/",AddPostView.as_view(),name="add_post"),
+]
