82bf4b5786eec47563e2defc2e8c5745d682948f
Walter Vianello <waltervianello@gmail.com>
2022-10-27 22:07:07 -0300

    Segunda parte

--- theblog/models.py
+++ theblog/models.py
@@ -1,19 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from datetime import datetime,date
 
 # Create your models here.
+
+class Category(models.Model):
+    name=models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("home")
+
+
 class Post(models.Model):
     title=models.CharField(max_length=255)
     title_tag=models.CharField(max_length=255)
     author=models.ForeignKey(User,on_delete=models.CASCADE)
     body=models.TextField()
+    post_date=models.DateField(auto_now_add=True)
+    category=models.CharField(max_length=255,default="no categorizado")
 
-def __str__(self):
-    return self.title + " I " + self.author
+    def __str__(self):
+        return self.title + " I " + self.author
 
-def get_absolute_url(self):
-    return reverse("home")
+    def get_absolute_url(self):
+        return reverse("home")
 
 
 
