6e7c216e4798cdf78fd8d42324b724e9c950a98f
Walter Vianello <waltervianello@gmail.com>
2022-11-01 02:27:18 -0300

    cuarto commit

--- theblog/forms.py
+++ theblog/forms.py
@@ -12,7 +12,7 @@ class PostForm(forms.ModelForm):
     class Meta:
 
         model=Post
-        fields=("title","title_tag","author","category","body")
+        fields=("title","title_tag","author","category","body","snippet","header_image")
         widgets={
             "title":forms.TextInput(attrs={"class":"form-control"}),
             "title_tag":forms.TextInput(attrs={"class":"form-control"}),
@@ -20,6 +20,7 @@ class PostForm(forms.ModelForm):
             #"author":forms.Select(attrs={"class":"form-control"}),
             "category":forms.Select(choices=choice_list,attrs={"class":"form-control"}),
             "body":forms.Textarea(attrs={"class":"form-control"}),
+            "snippet":forms.Textarea(attrs={"class":"form-control"}),
         }
 
 
--- theblog/models.py
+++ theblog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime,date
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 
@@ -17,12 +18,15 @@ class Category(models.Model):
 
 class Post(models.Model):
     title=models.CharField(max_length=255)
+    header_image=models.ImageField(null=True,blank=True, upload_to="images/")
     title_tag=models.CharField(max_length=255)
     author=models.ForeignKey(User,on_delete=models.CASCADE)
-    body=models.TextField()
+    body=RichTextField(blank=True, null=True)
     post_date=models.DateField(auto_now_add=True)
-    category=models.CharField(max_length=255,default="no categorizado")
+    category=models.CharField(max_length=255, default="No categorizado")
+    snippet=models.CharField(max_length=255)
     likes=models.ManyToManyField(User, related_name="blog_posts")
+    
 
     def total_likes(self):
         return self.likes.count()
